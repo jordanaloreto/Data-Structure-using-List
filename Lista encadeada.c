795b0cae21798eb8601d3163798c96016474fd68
Jordana Loreto <105024749+jordanaloreto@users.noreply.github.com>
2023-04-18 11:16:26 -0300

    Add files via upload

---
+++ Lista encadeada.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct lista
+{
+	int valor;
+	struct lista *proximo;
+};
+
+typedef struct lista Lista;
+
+Lista *lst_criar()
+{
+	return NULL;
+}
+
+Lista *lst_inserir(Lista *lst, int valor)
+{
+	Lista *novo = (Lista *) malloc(sizeof(Lista));
+	novo->valor = valor;
+	novo->proximo = lst;
+	
+	return novo; 
+}
+
+void lst_imprimir(Lista *lst)
+{
+	Lista *aux_lst;
+	
+	for(aux_lst = lst; aux_lst != NULL; aux_lst = aux_lst = aux_lst->proximo)
+	{
+		if(aux_lst == NULL)
+			printf("Nulo");
+		printf("%d\n", aux_lst->valor);
+	}
+}
+
+Lista *lst_buscar(Lista *lst, int valor)
+{
+	Lista *p;
+	
+	for(p = lst; p != NULL; p = p->proximo)
+	{
+		if (p->valor == valor)
+		{
+			return p;
+		}
+	}
+	
+	return NULL;
+	
+}
+
+Lista *lst_remover(Lista *lst, int v)
+{
+	Lista *p = lst;
+	Lista *ant = NULL;
+	
+	while(p != NULL && p->valor != v)
+	{
+		ant = p;
+		p = p->proximo;
+	}
+	
+	if(p == NULL)
+	{
+		return lst;
+	}
+	
+	if(ant == NULL)
+	{
+		lst = p->proximo;
+	}
+	else
+	{
+		ant->proximo = p->proximo;
+	}
+	
+	free(p);
+	return lst;
+	
+}
+
+void lst_liberar(Lista *lst)
+{
+	Lista *p = lst;
+	Lista *ant;
+		
+	while(p != NULL)
+	{
+		ant = p;
+		p = p->proximo;
+		free(ant);
+	}
+
+}
+
+int lst_vazia(Lista *lst)
+{
+	if(lst == NULL)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+Lista *lst_inserir_ordenado(Lista *l, int v)
+{
+	Lista *p = l;
+	Lista *ant = NULL;
+	while(p->valor<v&&p!=NULL)
+	{
+		ant = p;
+		p = p->proximo;
+	}
+	Lista *novo = (Lista *) malloc(sizeof(Lista));
+	novo->valor=v;
+	if(ant==NULL)
+	{
+		novo->proximo=l;
+		l=novo;
+	}
+	else
+	{
+		novo->proximo=ant->proximo;
+		ant->proximo=novo;
+	}
+	return l;
+	
+	
+	/*Lista *novo = (Lista *) malloc(sizeof(Lista));
+	novo->valor = valor;
+	novo->proximo = lst;
+	while(valor<novo)
+	{
+		for(p = lst; p != NULL; p = p->proximo)
+		{
+			if (p->valor == valor)
+			{
+			return p;
+			}
+		}
+	}
+	Lista *ant = NULL;
+	if(*ant==NULL)
+	{
+		novo->proximo=l;
+		l=novo;
+	}
+	else
+	{
+		ant->proximo=novo;
+		novo->proximo=p;
+	}
+	return l;*/
+}
+
+int main()
+{
+	Lista *lst1 = lst_criar();
+	
+	if(lst_vazia(lst1))
+	{
+		printf("Lista vazia\n");
+	}
+		
+	lst1 = lst_inserir_ordenado(lst1, 15);
+	lst1 = lst_inserir_ordenado(lst1, 27);
+	lst1 = lst_inserir_ordenado(lst1, 25);
+	lst1 = lst_inserir_ordenado(lst1, 13);
+	
+	if(lst_vazia(lst1))
+	{
+		printf("Lista vazia\n");
+	}
+	else
+	{
+		printf("Lista nao esta vazia\n");
+	}
+	
+	lst1 = lst_remover(lst1, 13);
+	
+	lst_imprimir(lst1);
+	
+	lst1 = lst_liberar(lst1);
+	
+	/*if(lst_vazia(lst1))
+	{
+		printf("Lista vazia");
+	}
+	
+	lst_imprimir(lst1);*/
+	
+	
+	return 0;
+}
